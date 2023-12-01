f8dc43a1cbc800dd6156f8720c05a351914d4110
Falkreon <falkreon@gmail.com>
2023-11-30 23:24:50 -0600

    Day 1

--- src/main/java/blue/endless/advent/App.java
+++ src/main/java/blue/endless/advent/App.java
@@ -1,8 +1,35 @@
 package blue.endless.advent;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class App {
+	
+	public static String sneakyData(String name) {
+		try {
+			return Files.readString(Path.of("src/data/"+name+".txt"));
+		} catch (IOException ex) {
+			System.out.println("Couldn't get data file for problem "+name);
+			System.exit(-1);
+		}
+		
+		return null;
+	}
+	
 	public static void main(String... args) {
+		if (args.length != 1) {
+			
+			System.out.println("usage: AdventOfCode2023 <day><a|b>");
+			System.out.println("example: AdventOfCode2023 3a");
+			System.exit(-1);
+		}
+		
+		switch(args[0]) {
+			case "1a" -> new Day1().a(sneakyData("1a"));
+			case "1b" -> new Day1().b(sneakyData("1a"));
 		
 		
+		}
 	}
 }
---
+++ src/main/java/blue/endless/advent/Day1.java
@@ -0,0 +1,89 @@
+package blue.endless.advent;
+
+public class Day1 {
+	public static final String EXAMPLE1 =
+			"""
+			1abc2
+			pqr3stu8vwx
+			a1b2c3d4e5f
+			treb7uchet
+			""";
+	
+	public static final String[] NUMBER_STRINGS = {
+		"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+	};
+	
+	public static int firstAndLastDigit(String s) {
+		int firstDigit = -1;
+		int lastDigit = -1;
+		
+		for(int i=0; i<s.length(); i++) {
+			char ch = s.charAt(i);
+			if (Character.isDigit(ch)) {
+				if (firstDigit == -1) firstDigit = Character.digit(ch, 10);
+				lastDigit = Character.digit(ch, 10);
+			}
+		}
+		
+		return Integer.valueOf("" + firstDigit + lastDigit);
+	}
+	
+	public static int firstAndLastDigitWithWords(String s) {
+		int firstIndex = -1;
+		int firstDigit = 0;
+		int lastIndex = -1;
+		int lastDigit = 0;
+		
+		for(int i=0; i<s.length(); i++) {
+			char ch = s.charAt(i);
+			if (Character.isDigit(ch)) {
+				if (firstIndex == -1) {
+					firstIndex = i;
+					firstDigit = Character.digit(ch, 10);
+				}
+				lastIndex = i;
+				lastDigit = Character.digit(ch, 10);
+			}
+		}
+		
+		//See if words preempt our existing indices
+		for(int i=0; i<NUMBER_STRINGS.length; i++) {
+			int index = s.indexOf(NUMBER_STRINGS[i]);
+			if (index > -1 && index < firstIndex) {
+				firstIndex = index;
+				firstDigit = i;
+			}
+			
+			index = s.lastIndexOf(NUMBER_STRINGS[i]);
+			if (index > -1 && index > lastIndex) {
+				lastIndex = index;
+				lastDigit = i;
+			}
+		}
+		
+		
+		return Integer.valueOf("" + firstDigit + lastDigit);
+	}
+	
+	public void a(String input) {
+		String[] lines = input.split("\n");
+		int sum = 0;
+		for(String line : lines) {
+			if (line.isBlank()) continue;
+			sum += firstAndLastDigit(line);
+		}
+		
+		System.out.println("Sum: "+sum);
+	}
+	
+	public void b(String input) {
+		String[] lines = input.split("\n");
+		int sum = 0;
+		for(String line : lines) {
+			if (line.isBlank()) continue;
+			sum += firstAndLastDigitWithWords(line);
+		}
+		
+		System.out.println("Sum: "+sum);
+	}
+}
